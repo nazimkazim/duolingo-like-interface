111c024998c8f3a73ad536cf626d30d6e72818ca
nazimkazim <nazik_111@mail.ru>
2020-09-18 15:01:07 +0600

    add answer

---
+++ data.js
@@ -0,0 +1,62 @@
+const data = [
+  {
+    pic: './assets/lara-woman.svg',
+    frPhrase: 'Tu parles japonais?',
+    engPhrase: 'Do you speak Japanese?',
+    phraseByWord: [
+      {
+        word: 'Tu',
+        trans: ['you']
+      },
+      {
+        word: 'parles',
+        trans: ['(you) speak', '(you) are speaking', '(you) are talking']
+      },
+      {
+        word: 'japonais',
+        trans: ['Japanese'],
+        highlighted: true
+      }
+    ]
+  },
+  {
+    pic: './assets/olga-woman.svg',
+    frPhrase: `Il vient d'Australie`,
+    engPhrase: 'He comes from Australia',
+    phraseByWord: [
+      {
+        word: 'Il',
+        trans: ['he', 'him', 'it']
+      },
+      {
+        word: 'vient d',
+        trans: ['come from', 'is from', 'has just'],
+        highlighted: true
+      },
+      {
+        word: 'Australie',
+        trans: ['Australia']
+      }
+    ]
+  },
+  {
+    pic: './assets/victor-male.svg',
+    frPhrase: `Je suis canadien`,
+    engPhrase: 'I am Canadian',
+    phraseByWord: [
+      {
+        word: 'Je',
+        trans: ['I']
+      },
+      {
+        word: 'suis',
+        trans: ['am']
+      },
+      {
+        word: 'canadien',
+        trans: ['Canadian'],
+        highlighted: true
+      }
+    ]
+  }
+];
--- main.js
+++ main.js
@@ -1,85 +1,62 @@
-/* import LaraWoman from './assets/lara-woman.svg';
-import OlgaWoman from './assets/olga-woman.svg';
-import VictorMale from './assets/victor-male.svg'; */
-
-const data = [
-  {
-    pic: './assets/lara-woman.svg',
-    frPhrase: 'Tu parles japonais?',
-    engPhrase: 'Do you speak Japanese?',
-    phraseByWord: [
-      {
-        word: 'Tu',
-        trans: ['you']
-      },
-      {
-        word: 'parles',
-        trans: ['(you) speak', '(you) are speaking', '(you) are talking']
-      },
-      {
-        word: 'japonais',
-        trans: ['Japanese'],
-        highlighted: true
-      }
-    ]
-  },
-  {
-    pic: './assets/olga-woman.svg',
-    frPhrase: `Il vient d'Australie`,
-    engPhrase: 'He comes from Australia',
-    phraseByWord: [
-      {
-        word: 'Il',
-        trans: ['he', 'him', 'it']
-      },
-      {
-        word: 'vient d',
-        trans: ['come from', 'is from', 'has just'],
-        highlighted: true
-      },
-      {
-        word: 'Australie',
-        trans: ['Australia']
-      }
-    ]
-  },
-  {
-    pic: './assets/victor-male.svg',
-    frPhrase: `Je suis canadien`,
-    engPhrase: 'I am Canadian',
-    phraseByWord: [
-      {
-        word: 'Je',
-        trans: ['I']
-      },
-      {
-        word: 'suis',
-        trans: ['am']
-      },
-      {
-        word: 'canadien',
-        trans: ['Canadian'],
-        highlighted: true
-      }
-    ]
-  }
-];
-
 const board = document.querySelector('.board');
 const tts = window.speechSynthesis;
-let incr = 0;
+let incr = 2;
+const randomWordsArr = [];
+const answer = [];
+
+
+data.map((obj) => {
+  splittedStr = obj.engPhrase.split(" ");
+  return randomWordsArr.push(...splittedStr);
+});
+
+//console.log('line 11', randomWordsArr);
+
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
 
 
 board.innerHTML =
   `
   <div class="target-lang-container">
-  <img src=${data[incr].pic} />
-  <div class="speech-bubble">
-    <div class="sent-container"><img id="speaker" src="/assets/speaker.svg" />
-    ${data[incr].phraseByWord.map((word) => {
+    <img src=${data[incr].pic} />
+    <div class="speech-bubble">
+      <div class="sent-container">
+        <img id="speaker" src="/assets/speaker.svg" />
+      ${data[incr].phraseByWord.map((word) => {
     return `<button value=${word.word} class="word-item ${word.highlighted && 'highlighted'}">${word.word}</button>`;
   }).join(' ')}
-    </div</div></div>`;
+      </div>
+    </div>
+  </div>
+  <div class="answer-field">
+    ${answer.map((ans) => (
+      `<button value=${ans} class="word">${ans}</button>`
+    ))}
+  </div>
+  <div class="random-words-field">
+    ${randomWordsArr.map((item) => (
+    `<button value=${item} class="word">${item}</button>`
+  )).join(' ')
+  }
+  </div>
+  `;
 
 
 const speaker = document.getElementById('speaker');
@@ -95,11 +72,17 @@ speaker.addEventListener('click', () => {
 });
 
 const wordItems = document.querySelectorAll('.word-item');
+const word = document.querySelectorAll('.word');
 
 //console.log(wordItems)
 
 wordItems.forEach(wordItem => wordItem.addEventListener('mouseover', (e) => {
   //console.log(e.target.value);
   speak(e.target.value, 'fr-FR');
+}));
+
+word.forEach(item => item.addEventListener('click', (e) => {
+  answer.push(e.target.value)
+  console.log(e.target.value);
 }));
 
