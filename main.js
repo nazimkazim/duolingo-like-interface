9abe98d1a1cc1ee1f579cee466b7d032d12b3971
nazimkazim <nazik_111@mail.ru>
2020-09-20 12:51:16 +0600

    add fix incr

--- main.js
+++ main.js
@@ -1,111 +1,106 @@
-const board = document.querySelector('.board');
-const avatarImg = document.querySelector('.avatar-img');
-const sentContainer = document.querySelector('.sent-container');
-const speakerEl = document.createElement('img');
-const word = document.querySelectorAll('.word');
-speakerEl.setAttribute('id', 'speaker')
-speakerEl.src = '/assets/speaker.svg';
+window.addEventListener('load', function () {
+  const board = document.querySelector('.board');
+  const avatarImg = document.querySelector('.avatar-img');
+  const sentContainer = document.querySelector('.sent-container');
+  const speakerEl = document.createElement('img');
+  const word = document.querySelectorAll('.word');
+  speakerEl.setAttribute('id', 'speaker');
+  speakerEl.src = '/assets/speaker.svg';
 
-const tts = window.speechSynthesis;
-let incr = 0;
-const randomWordsArr = [];
-const answer = [];
+  const tts = window.speechSynthesis;
+  let incr = 0;
+  const randomWordsArr = [];
+  const answer = [];
 
 
-data.map((obj) => {
-  splittedStr = obj.engPhrase.split(" ");
-  return randomWordsArr.push(...splittedStr);
-});
-
-//console.log('line 11', randomWordsArr);
-
-function shuffle(array) {
-  var currentIndex = array.length, temporaryValue, randomIndex;
-
-  // While there remain elements to shuffle...
-  while (0 !== currentIndex) {
-
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
-
-    // And swap it with the current element.
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
-  }
-
-  return array;
-}
-
-const incrementer = () => {
-  return incr += 1;
-};
-
-const resetQuestion = () => {
-  sentContainer.innerHTML = ''
-}
-
-const iterateEachWord = (i) => {
-  const questions = [...data];
-  //console.log(questions[i])
-  avatarImg.src = questions[i].pic;
-  sentContainer.appendChild(speakerEl)
-  questions[i].phraseByWord.map((word) => {
-    const wordEl = document.createElement('button');
-    wordEl.innerText = word.word;
-    wordEl.setAttribute('class', 'word-item')
-    wordEl.setAttribute('value', word.word)
-    sentContainer.appendChild(wordEl)
+  data.map((obj) => {
+    splittedStr = obj.engPhrase.split(" ");
+    return randomWordsArr.push(...splittedStr);
   });
-};
 
-const getNewQuestion = (i) => {
-  const questions = [...data];
-  //console.log(questions[i])
-  avatarImg.src = questions[i].pic;
-  resetQuestion();
-  iterateEachWord(i)
-};
+  //console.log('line 11', randomWordsArr);
 
+  function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
 
-const speak = (phrase, lang) => {
-  let toSpeak = new SpeechSynthesisUtterance(phrase);
-  toSpeak.lang = lang;
-  tts.speak(toSpeak);
-};
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
 
-speakerEl.addEventListener('click', () => {
-  speak(data[incr].frPhrase, 'fr-FR');
-});
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
 
-//console.log(wordItems)
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
 
-
-
-function answerFunc(val) {
-  return answer.push(val);
-}
-
-word.forEach(item => item.addEventListener('click', (e) => {
-  answerFunc(e.target.value);
-  console.log(e.target.value);
-}));
-
-window.addEventListener('keydown', (key) => {
-  if (key.keyCode == "39") {
-    let i = incrementer();
-    getNewQuestion(i);
-    console.log(i);
+    return array;
   }
+
+  const incrementer = () => {
+    return incr += 1;
+  };
+
+  const resetQuestion = () => {
+    sentContainer.innerHTML = '';
+  };
+
+  const iterateEachWord = (i) => {
+    const questions = [...data];
+    //console.log(questions[i])
+    avatarImg.src = questions[i].pic;
+    sentContainer.appendChild(speakerEl);
+    questions[i].phraseByWord.map((word) => {
+      let wordEl = document.createElement('button');
+      wordEl.innerText = word.word;
+      wordEl.setAttribute('class', 'word-item');
+      wordEl.setAttribute('value', word.word);
+      sentContainer.appendChild(wordEl);
+    });
+  };
+
+  const getNewQuestion = (i) => {
+    const questions = [...data];
+    //console.log(questions[i])
+    avatarImg.src = questions[i].pic;
+    resetQuestion();
+    iterateEachWord(i);
+    const wordItems = document.querySelectorAll('.word-item');
+    wordItems.forEach(wordItem => wordItem.addEventListener('mouseover', (e) => {
+      console.log(e.target.value);
+      speak(e.target.value, 'fr-FR');
+    }));
+  };
+
+
+  const speak = (phrase, lang) => {
+    let toSpeak = new SpeechSynthesisUtterance(phrase);
+    toSpeak.lang = lang;
+    tts.speak(toSpeak);
+  };
+
+  speakerEl.addEventListener('click', () => {
+    speak(data[incr].frPhrase, 'fr-FR');
+  });
+
+  //console.log(wordItems)
+
+  window.addEventListener('keydown', (key) => {
+    if (key.keyCode == "39") {
+      incr += 1
+      getNewQuestion(incr);
+    }
+    if (key.keyCode == "37") {
+      incr -= 1
+      getNewQuestion(incr);
+    }
+  });
 });
 
-const wordItems = document.querySelectorAll('.word-item');
 
-wordItems.forEach(wordItem => wordItem.addEventListener('mouseover', (e) => {
-  console.log(e.target.value);
-  speak(e.target.value, 'fr-FR');
-}));
+
 
 
 
