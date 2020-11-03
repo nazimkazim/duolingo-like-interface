d431a6fde3a87ee1bcb72dca5057b920edc26628
nazimkazim <nazik_111@mail.ru>
2020-11-03 15:06:53 +0600

    add remove unnecessary code

--- main.js
+++ main.js
@@ -17,16 +17,15 @@ window.addEventListener('load', function () {
   let answerFieldWordsArr = [];
   //let answerWordsArrIsFull = false;
 
-  checkButton.addEventListener('click', () => {
-    console.log('clicked');
-  });
+  console.log('random words arr', randomWordsArr)
 
-
+  
   data.map((obj) => {
     splittedStr = obj.engPhrase.split(" ");
     return randomWordsArr.push(...splittedStr);
   });
   //console.log(randomWordsArr);
+  console.log('random words arr', randomWordsArr)
 
   shuffle(randomWordsArr).map((randWord) => {
     const el = document.createElement('button');
@@ -37,14 +36,11 @@ window.addEventListener('load', function () {
     randomWordsField.appendChild(el);
   });
 
-  //const randomWordsList = document.querySelectorAll('.word');
-  //console.log(randomWordsList);
-
-  //console.log(randomWordsArrDom);
-
   randomWordsArrDom.forEach(w => {
     randomWordsField.appendChild(w);
   });
+
+  console.log('random words dom', randomWordsArrDom);
 
   function randomEls() {
     let arr = Array.from(randomWordsField.children);
@@ -54,18 +50,6 @@ window.addEventListener('load', function () {
         answerField.appendChild(e.target);
         //console.log(e.target)
         let index = randomWordsArrDom.indexOf(e.target);
-        //console.log(index)
-        //randomWordsArrDom.splice(index, 1);
-        //console.log(randomWordsArrDom)
-        /* if (answerWordsArrDom.length > randomWordsArr.length || answerWordsArrDom.includes(e.target)) {
-          answerWordsArrIsFull = true;
-        } */
-
-        /* if (!answerWordsArrIsFull) {
-          answerWordsArrDom.push(e.target);
-        } */
-        //console.log(answerWordsArrDom);
-        //console.log(randomWordsArrDom);
         upd();
         arr.splice(index, 1);
         console.log(randomWordsField.children);
@@ -74,19 +58,6 @@ window.addEventListener('load', function () {
   }
 
   randomEls();
-
-
-  /* answerWordsArrDom.forEach(w => {
-    showWords();
-    console.log('hey');
-    w.addEventListener('click', (e) => {
-      console.log(e.target);
-      console.log('hhh');
-      let index = answerWordsArrDom.indexOf(e.target);
-      answerWordsArrDom.splice(index, 1);
-      randomWordsField.appendChild(e.target);
-    });
-  }); */
 
   //console.log(answerField.children.length) 
   function upd() {
@@ -112,92 +83,7 @@ window.addEventListener('load', function () {
       checkButton.disabled = true;
       checkButton.classList.remove("enabled-check-button");
     }
-    /* if (answerFieldWordsArr.length === 1) {
-      console.log('empty')
-      answerFieldWordsArr.length = 0
-      answerField.children.innerHTML = ''
-      randomEls()
-      console.log(answerFieldWordsArr)
-    } */
   }
-
-
-
-  /* randomWordsList.forEach(w => {
-    // create empty array to push list nodes value
-    let wordsListRandomArr = [];
-    let anwersArr = [];
-    w.addEventListener('click', (e) => {
-      // define value we click
-      let val = e.target.value;
-      // get list nodes of random words field
-      let listNodes = randomWordsField.childNodes;
-      // make array of nodes
-      let listNodesArr = Array.from(listNodes);
-      // iterate nodes and push values to wordsListRandomArr
-      listNodesArr.map((el, index) => {
-        //console.log(el.value)
-        wordsListRandomArr.push({ val: el.value, idx: index });
-        //console.log(wordsListRandomArr);
-      });
-
-      console.log(wordsListRandomArr);
-
-      // get index func
-      function getVal(v) {
-        let val = wordsListRandomArr.filter(obj => {
-          return obj.val === v;
-        });
-
-        return val[0].idx;
-      }
-
-      // get index of clicked word
-      let idx = getVal(val);
-      console.log(idx);
-      // remove words from answer field by index
-      randomWordsField.removeChild(randomWordsField.childNodes[idx]);
-      //wordsListRandomArr.splice(idx, 1)
-      console.log(wordsListRandomArr);
-      // get word that is going to append to answer field 
-      let appendedWord = wordsListRandomArr[idx].val;
-      //console.log(appendedWord)
-      //console.log(answerField)
-      // create button el
-      let el = document.createElement("button");
-      // assign class
-      el.className = 'word-answer';
-
-      el.setAttribute('value', val);
-      // assign text
-      el.innerText = appendedWord;
-      // append word to answer field                              
-      answerField.appendChild(el);
-
-      anwersArr.push({ val: val, idx: idx })
-
-      console.log(anwersArr)
-
-      let answerWordsList = document.querySelectorAll('.word-answer');
-      answerWordsList.forEach(w => {
-        w.addEventListener('click', (e) => {
-          let val = e.target.value;
-          wordsListRandomArr.push(val);
-          let el = document.createElement("button");
-          // assign class
-          el.className = 'word';
-
-          el.setAttribute('value', val);
-          // assign text
-          el.innerText = appendedWord;
-          // append word to answer field                              
-          randomWordsField.appendChild(el);
-          console.log(wordsListRandomArr);
-        });
-      });
-
-    });
-  }); */
 
   function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
